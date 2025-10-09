52f7ef4342f7f6d4444cc609f4be58e14220b66a
Cabrel <cabrelk9@gmail.com>
2025-10-09 12:41:18 +0100

    Fix typo in test, use root_url

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get route_url
+    get rout_url
     assert_response :success
   end
 end
